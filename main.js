de42d0ce08d68db146a24f3f41e04ab6dfa91ffc
Andrew Zheng <andaw820@gmail.com>
2022-08-06 17:01:30 -0400

    second save

---
+++ main.js
@@ -0,0 +1,17 @@
+const input = document.querySelector(".input");
+
+const apiData = {
+    url: "https://v2.jokeapi.dev/joke/",
+    cate: ""
+  };
+
+let dataUrl = apiData.url;
+
+function cateBtn(category){
+    dataUrl+=category;
+    console.log(dataUrl);
+    fetch("https://v2.jokeapi.dev/joke/Programming")
+        .then((results) => results.json())
+        .then(console.log);
+
+}
